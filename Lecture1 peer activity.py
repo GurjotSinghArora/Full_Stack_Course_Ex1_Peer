2017a2e3c920b19412c00535e409dc0143e40569
GurjotSinghArora <94769087+GurjotSinghArora@users.noreply.github.com>
2022-09-13 19:52:42 -0400

    Add files via upload

---
+++ Lecture1 peer activity.py
@@ -0,0 +1,29 @@
+first_number=input("Please enter the first number: ")
+second_number=input("Please enter the second number: ")
+
+#implementing addition
+
+add= int(first_number)+int(second_number)
+
+#subtraction
+
+sub=int(first_number)-int(second_number)
+
+# multiplication
+
+multi=int(first_number)*int(second_number)
+
+# power
+
+power=int(first_number)**int(second_number)
+
+#division
+
+div=int(first_number)/int(second_number)
+
+
+print("The addition of two numbers is: " + str(add))
+print("The subtraction of two numbers is: " + str(sub))
+print("The multiplication of two numbers is: " + str(add))
+print("The power of two numbers is: " + str(power))
+print("The division of two numbers is: " + str(div))
