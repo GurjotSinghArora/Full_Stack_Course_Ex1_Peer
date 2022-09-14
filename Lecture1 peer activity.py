6d20988217b06127cf9708fbeb9e398568203b5e
rajsaurav4055 <113482311+rajsaurav4055@users.noreply.github.com>
2022-09-13 20:24:44 -0400

    Update Lecture1 peer activity.py

--- Lecture1 peer activity.py
+++ Lecture1 peer activity.py
@@ -24,6 +24,37 @@ div=int(first_number)/int(second_number)
 
 print("The addition of two numbers is: " + str(add))
 print("The subtraction of two numbers is: " + str(sub))
-print("The multiplication of two numbers is: " + str(add))
+print("The multiplication of two numbers is: " + str(multi))
 print("The power of two numbers is: " + str(power))
 print("The division of two numbers is: " + str(div))
+
+#Saurav's (Peer) edit
+
+third_number = input("Please enter a third number: ")
+#implementing addition
+
+final_add= int(first_number)+int(second_number)+ int(third_number)
+
+#subtraction
+
+final_sub=int(sub)-int(third_number)
+
+# multiplication
+
+final_multi=int(multi)*int(third_number)
+
+# power
+
+final_power=int(power)**int(third_number)
+
+#division
+
+final_div=int(div)/int(third_number)
+
+
+print("The final addition value is: " + str(final_add))
+print("The final subtraction value is: " + str(final_sub))
+print("The final multiplication value is: " + str(final_multi))
+print("The final power is: " + str(final_power))
+print("The final division is: " + str(final_div))
+
